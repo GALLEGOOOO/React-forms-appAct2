e9da53fca7eee9e75a32e98a70323f56aa34a3ae
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-03-12 20:13:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import { Filter, Numbers, PersonForm } from "./components";
-import { useFormFilter, usePersons, useForm } from "./hooks";
+import React, { useState } from 'react';
+import Filter from './Filter';
+import PersonForm from './PersonForm';
+import Persons from './Persons';
 
 const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-	const { newName, newNumber, handleNewNameValue, handleNewNumberValue } = useForm();
-	const { newFilter, handleNewFilterValue } = useFormFilter();
-	const { persons, handleChangePersonsValue} = usePersons();
-	
-	return (
-		<div>
-			<h2>Phonebook</h2>
-			<Filter setNewFilter={ handleNewFilterValue }/>
-			<PersonForm 
-				newName={ newName } 
-				setNewName={ handleNewNameValue } 
-				newNumber={ newNumber }
-				handleNewNumber={ handleNewNumberValue }
-				persons={ persons }
-				setPersons={ handleChangePersonsValue }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-			/>
-			<Numbers newFilter={ newFilter } persons={ persons }/>
-		</div>
-	);
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm persons={persons} setPersons={setPersons} />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={filteredPersons} />
+    </div>
+  );
 };
 
 export default App;
